7d2da67a4381058d3b3c8f5689b8e7e36aec18aa
GhostGamer0o <kendiquit@yahoo.com>
2025-05-14 17:56:54 -0400

    Updated Code

--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,6 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
 
+    public enum ControlMode
+    {
+        Seek,
+        Pursue,
+        Food,
+        Obstacle
+    }
+
+    public ControlMode controlMode = ControlMode.Seek;
+
+    public GameObject boidPrefab = null;
+    public int numBoidsToSpawn = 10;
+    public List<Boid> boids = null;
+
+    private void Start()
+    {
+
+        for (int i = 0; i < numBoidsToSpawn; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
+            Quaternion rotation = Random.rotation;
+
+            GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
+            boids.Add(spawnedBoid.GetComponent<Boid>());
+
+            spawnedBoid.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0f, 1f, 0.5f, 1f));
+
+            spawnedBoid.GetComponent<Rigidbody>().linearVelocity = Random.onUnitSphere * 0.7f;
+
+            spawnedBoid.transform.localScale *= Random.Range(0.9f, 3f);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlMode = ControlMode.Seek;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlMode = ControlMode.Pursue;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            controlMode = ControlMode.Food;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            controlMode = ControlMode.Obstacle;
+        }
+    }
 }
