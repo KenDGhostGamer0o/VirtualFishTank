49eecdbaff245dd102e0a74c13a366a3ac31ac1c
GhostGamer0o <kendiquit@yahoo.com>
2025-06-09 21:50:02 -0400

    Updated Scripts

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,4 +2,64 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+
+    public float speedMax = 2;
+    public float accelMax = 3;
+    internal readonly float acceleration;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, rigidbody.linearVelocity, Color.red);
+    }
+
+    public Vector3 Seek(Vector3 target, float acceleration)
+    {
+        Vector3 toTarget = target - transform.position;
+
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        Vector3 accel = toTargetNormalized * acceleration;
+
+        return accel;
+    }
+
+    public Vector3 Pursue(Vector3 target, float acceleration, float desiredSpeed)
+    {
+        Vector3 toTarget = target - transform.position;
+
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        Vector3 desiredVelocity = toTargetNormalized * desiredSpeed;
+
+        Vector3 deltaVel = desiredVelocity - rigidbody.linearVelocity;
+
+        Vector3 accel = toTargetNormalized * acceleration;
+
+        return accel;
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = rigidbody.linearVelocity.magnitude;
+
+        if (speed > speedMax)
+        {
+            rigidbody.linearVelocity = rigidbody.linearVelocity * speedMax / speed;
+        }
+    }
+    private void Awake()
+    {
+        GetComponent<Renderer>().material.SetColor(" BaseColor ", Random.ColorHSV(0, 1, 0f, 1f, 0.5f, 1f));
+    }
+
+    internal Vector3 Seek(Vector3 position, Boid boid)
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoidSimulationControl : MonoBehaviour
 {
@@ -15,11 +16,45 @@ public class BoidSimulationControl : MonoBehaviour
     public ControlMode controlMode = ControlMode.Seek;
 
     public GameObject boidPrefab = null;
+    public GameObject foodPrefab = null;
+    public GameObject targetObject = null;
     public int numBoidsToSpawn = 10;
     public List<Boid> boids = null;
 
+    private void SeekModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].acceleration);
+
+            if (Input.GetMouseButton(0))
+            {
+                boids[i].rigidbody.linearVelocity += accel * Time.fixedDeltaTime;
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+            }
+            else if (Input.GetMouseButton(1))
+            {
+                boids[i].rigidbody.linearVelocity -= accel * Time.fixedDeltaTime;
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+            }
+
+        }
+
+    }
+
+    private void PursueModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            Vector3 accel = boids[i].Pursue(targetObject.transform.position, boids[i].accelMax, boids[i].speedMax);
+            boids[i].rigidbody.linearVelocity += accel * Time.fixedDeltaTime;
+            Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+        }
+    }
+
     private void Start()
     {
+        targetObject = GameObject.Find("target");
 
         for (int i = 0; i < numBoidsToSpawn; i++)
         {
@@ -27,7 +62,12 @@ public class BoidSimulationControl : MonoBehaviour
             Quaternion rotation = Random.rotation;
 
             GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
-            boids.Add(spawnedBoid.GetComponent<Boid>());
+
+            Boid boidComponent = spawnedBoid.GetComponent<Boid>();
+            boidComponent.speedMax = Random.Range(0.5f, 1.5f);
+            boidComponent.accelMax = Random.Range(0.5f, 1.5f);
+
+            boids.Add(boidComponent);
 
             spawnedBoid.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0f, 1f, 0.5f, 1f));
 
@@ -55,5 +95,65 @@ public class BoidSimulationControl : MonoBehaviour
         {
             controlMode = ControlMode.Obstacle;
         }
+
+        if (Input.GetMouseButtonDown(0) && controlMode == ControlMode.Food)
+        {
+            SpawnFood();
+        }
+
+    }
+
+    private void SpawnFood()
+    {
+        Instantiate(foodPrefab, targetObject.transform.position, Random.rotation);
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            float foodSeekRadius = 0.5f;
+            Collider[] colliders = Physics.OverlapSphere(boids[i].transform.position, foodSeekRadius);
+            foreach (Collider collider in colliders)
+            {
+                Food food = collider.GetComponent<Food>();
+                if (food != null)
+                {
+                    Vector3 accel = boids[i].Seek(collider.transform.position, boids[i]);
+                    boids[i].rigidbody.linearVelocity += accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                }
+            }
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+        bool didhit = Physics.Raycast(ray, out hitinfo, 100);
+
+
+        if (didhit)
+        {
+            targetObject.transform.position = hitinfo.point;
+        }
+
+        switch (controlMode)
+        {
+            case ControlMode.Seek:
+                {
+                    SeekModeControl();
+                    break;
+                }
+
+            case ControlMode.Pursue:
+                {
+                    PursueModeControl();
+                    break;
+                }
+        }
     }
 }
+
+
+
